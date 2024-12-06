c3681b3529360f3ad9c1c421ee1426250d510695
aitorfi <aitorfidalgo@gmail.com>
2024-12-06 13:14:39 +0100

    JWT Stateless validation

--- user_management_api/api/views.py
+++ user_management_api/api/views.py
@@ -1,12 +1,12 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes  
-from rest_framework_simplejwt.authentication import JWTAuthentication 
+from rest_framework_simplejwt.authentication import JWTStatelessUserAuthentication 
 
 from . import views_match_history
 
 @api_view(['GET'])
-@authentication_classes([JWTAuthentication])
+@authentication_classes([JWTStatelessUserAuthentication])
 def stats_view(request, user_id):
     try:
         return views_match_history.stats_view(request, user_id)
@@ -14,7 +14,7 @@ def stats_view(request, user_id):
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
-@authentication_classes([JWTAuthentication])
+@authentication_classes([JWTStatelessUserAuthentication])
 def match_list_id(request, pk):
     try:
         return views_match_history.match_list_id(request, pk)
@@ -22,7 +22,7 @@ def match_list_id(request, pk):
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
-@authentication_classes([JWTAuthentication])
+@authentication_classes([JWTStatelessUserAuthentication])
 def match4_list_id(request, pk):
     try:
         return views_match_history.match4_list_id(request, pk)
